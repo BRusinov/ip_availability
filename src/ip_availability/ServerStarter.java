3574db527cb730d4744837c797b605799d4cf1ac
BRusinov <borislav.rusinov@gmail.com>
2015-11-14 18:14:15 +0200

    Changed server port because of my stupidity

---
+++ src/ip_availability/ServerStarter.java
@@ -0,0 +1,14 @@
+package ip_availability;
+
+import java.io.IOException;
+
+
+public class ServerStarter {
+
+		private static final int SERVER_PORT = 40002;
+
+		public static void main(String[] args) throws IOException {
+			final EchoServer server=new EchoServer(SERVER_PORT);
+			server.startServer();
+		}
+}
