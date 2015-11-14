e5b4e96d71d04761eef48543a3e32b4b36286d42
BRusinov <borislav.rusinov@gmail.com>
2015-11-14 13:15:35 +0200

    Adding interface currentlyLoggedUsers.

---
+++ src/ip_availability/currentlyLoggedUsers.java
@@ -0,0 +1,11 @@
+package ip_availability;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public interface currentlyLoggedUsers {
+	final List<String> currentlyLoggedUsers = new LinkedList<String>();
+	final Map<String, Integer> usersToLoginCount = new HashMap<String, Integer>();
+}
