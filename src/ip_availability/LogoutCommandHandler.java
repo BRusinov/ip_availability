c9a3006aebaa69716266d67acb5403aeb8db9068
BRusinov <borislav.rusinov@gmail.com>
2015-11-14 18:12:20 +0200

    Adding LogoutCommand functionality

--- src/ip_availability/LogoutCommandHandler.java
+++ src/ip_availability/LogoutCommandHandler.java
@@ -1,27 +1,29 @@
 package ip_availability;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+
 public class LogoutCommandHandler implements currentlyLoggedUsers {
 	private final String command;
+	private final Socket socket;
 	
 	
-	public LogoutCommandHandler(String command){
+	public LogoutCommandHandler(String command, Socket socket){
 		this.command=command;
+		this.socket=socket;
 	}
 	
-	public Boolean Logout(){
-		final String[] split = command.split(":");
-		
-		if("logout".equals(split[1])){
-			if(currentlyLoggedUsers.contains(split[0]) && usersToLoginCount.containsKey(split[0])){
-				currentlyLoggedUsers.remove(split[0]);
-				return true;
-			}
-			else {
-				System.out.println("error:notlogged");
-				return true;
-			}
+	public Boolean Logout(String string) throws IOException{
+		final String[] split = string.split(":");
+		final PrintStream out = new PrintStream(socket.getOutputStream());
+		if(currentlyLoggedUsers.contains(split[0]) && usersToLoginCount.containsKey(split[0])){
+			currentlyLoggedUsers.remove(split[0]);
+			return true;
 		}
-		System.out.println("error:unknowncommand");
-		return true;
+		else {
+			out.println("error:notlogged");
+			return true;
+		}
 	}
 }
