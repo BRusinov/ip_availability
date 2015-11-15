bada27b9209c97ee10aa111384eebd62e3a6b798
BRusinov <borislav.rusinov@gmail.com>
2015-11-15 16:18:12 +0200

    Aaaand again changed the port because i tried to run the server when it has already been run.

--- src/ip_availability/ServerStarter.java
+++ src/ip_availability/ServerStarter.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class ServerStarter {
 
-		private static final int SERVER_PORT = 40004;
+		private static final int SERVER_PORT = 40005;
 
 		public static void main(String[] args) throws IOException {
 			final EchoServer server=new EchoServer(SERVER_PORT);
