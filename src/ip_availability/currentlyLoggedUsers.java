68173400cf1ae2d825afb2327c9561c51fc720f8
BRusinov <borislav.rusinov@gmail.com>
2015-11-15 11:27:50 +0200

    Added new list - notloggedusers. It is for users who, right now, are not in the system, but were in before.

--- src/ip_availability/currentlyLoggedUsers.java
+++ src/ip_availability/currentlyLoggedUsers.java
@@ -8,4 +8,5 @@ import java.util.Map;
 public interface currentlyLoggedUsers {
 	final List<String> currentlyLoggedUsers = new LinkedList<String>();
 	final Map<String, Integer> usersToLoginCount = new HashMap<String, Integer>();
+	final static List<String> notLoggedUsers=new LinkedList<String>();
 }
