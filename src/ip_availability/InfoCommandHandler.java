9c2860cc49fcf1334a816cd1b052d784149b2f3b
BRusinov <borislav.rusinov@gmail.com>
2015-11-15 15:20:22 +0200

    Same as for ShutdownCommandHandler

--- src/ip_availability/InfoCommandHandler.java
+++ src/ip_availability/InfoCommandHandler.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
 
-public class InfoCommandHandler implements currentlyLoggedUsers {
+public class InfoCommandHandler implements General {
 	private final String command;
 	private final Socket socket;
 	public InfoCommandHandler(String command, Socket socket){
@@ -12,17 +12,14 @@ public class InfoCommandHandler implements currentlyLoggedUsers {
 		this.socket=socket;
 	}
 	
-	public Boolean Info(String string) throws IOException{
+	public void Info(String string) throws IOException{
 		final String[] split = string.split(":");
 		final PrintStream out = new PrintStream(socket.getOutputStream());
-
 		if(currentlyLoggedUsers.contains(split[0])){
 			out.println("ok:" + split[2] + ":" + (currentlyLoggedUsers.contains(split[2]) ? "true" : "false") + ":" + usersToLoginCount.get(split[2]));
-			return true;
 		}
 		else {
 			out.println("error:notlogged");
-			return true;
 		}
 	}
 	
