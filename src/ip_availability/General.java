62bca684b82a2180379836e6a972a1bd341682d4
BRusinov <borislav.rusinov@gmail.com>
2015-11-15 15:18:50 +0200

    changed notloggeduser(list) to static.

--- src/ip_availability/General.java
+++ src/ip_availability/General.java
@@ -5,8 +5,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-public interface currentlyLoggedUsers {
-	final List<String> currentlyLoggedUsers = new LinkedList<String>();
+public interface General {
+	final static List<String> currentlyLoggedUsers = new LinkedList<String>();
 	final Map<String, Integer> usersToLoginCount = new HashMap<String, Integer>();
 	final static List<String> notLoggedUsers=new LinkedList<String>();
 }
