1f74d05e031c27aeb856ba3130136600f3fc17de
BRusinov <borislav.rusinov@gmail.com>
2015-11-15 15:20:44 +0200

    Same as for ShutdownCommandHandler

--- src/ip_availability/LogoutCommandHandler.java
+++ src/ip_availability/LogoutCommandHandler.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
 
-public class LogoutCommandHandler implements currentlyLoggedUsers {
+public class LogoutCommandHandler implements General {
 	private final String command;
 	private final Socket socket;
 	
