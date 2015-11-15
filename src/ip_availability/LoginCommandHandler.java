b1734fd1583c69e83371d3f4cf9c777613632cf4
BRusinov <borislav.rusinov@gmail.com>
2015-11-15 15:17:17 +0200

    removed unneccessary logic.

--- src/ip_availability/LoginCommandHandler.java
+++ src/ip_availability/LoginCommandHandler.java
@@ -4,26 +4,18 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
 
-public class LoginCommandHandler implements currentlyLoggedUsers{
+public class LoginCommandHandler implements General{
 	private final String command;
 	private final Socket socket;
-	
 	public LoginCommandHandler(String command, Socket socket) {
 		this.command=command;
 		this.socket=socket;
 	}
-	
-	
 	public void Login(String string) throws IOException{
 		final String[] split = string.split(":");
 		final PrintStream out = new PrintStream(socket.getOutputStream());
 		if(currentlyLoggedUsers.contains(split[1])) {				
-			if (usersToLoginCount.containsKey(split[1])){
-				usersToLoginCount.put(split[1], usersToLoginCount.get(split[1]) + 1);
-				notLoggedUsers.remove(split[1]);
-			}
-			else{
-				usersToLoginCount.put(split[1], 1);
+			if (notLoggedUsers.contains(split[1])){
 				notLoggedUsers.remove(split[1]);
 			}
 			out.println("ok");
